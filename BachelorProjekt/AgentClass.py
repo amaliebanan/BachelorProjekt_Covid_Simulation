72e6900f6d7315a5ff64a4dc2694601d6271548d
amaliebanan <qtb931@alumni.ku.dk>
2021-02-10 21:10:28 +0100

    Main added, schedule and step-function added

--- BachelorProjekt/AgentClass.py
+++ BachelorProjekt/AgentClass.py
@@ -3,18 +3,15 @@ import numpy as np
 
 
 class covid_Agent(Agent):
-    def __init__(self,id,infected,model):
-        super().__init__(self,id,model)
-        self.infected = infected
+    def __init__(self,id, model):
+        super().__init__(id, model)
+        self.infected = False
+        self.id = id
 
+    #The step method is the action the agent takes when it is activated by the model schedule.
     def step(self):
         #to be implemented
         #whatever the agent does when activated
-        print("what to do?")
-
-agent1 = covid_Agent(0,False)
-agent2 = covid_Agent(1,True)
-
-agent1.infected = not agent1.infected
+        print("Hi, my ID is",str(self.id))
 
 
--- BachelorProjekt/Model.py
+++ BachelorProjekt/Model.py
@@ -6,5 +6,15 @@ from mesa import Agent, Model
 class covid_Model(Model):
     def __init__(self,N):
         self.n_agents = N
-        for i in range(0,N):
-            newAgent = ac.Agent(i,False,self)
+        self.schedule = RandomActivation(self)
+        #The scheduler is a special model component which controls the order in which agents are activated
+        for i in range(self.n_agents):
+            newAgent = ac.covid_Agent(i,self)
+            #add agent to scheduler
+            self.schedule.add(newAgent)
+
+    def step(self):
+        self.schedule.step()
+
+myModel = covid_Model(10)
+myModel.step()
