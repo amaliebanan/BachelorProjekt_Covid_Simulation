679aec2da23c86763aebb8bf32eb354a56e3a732
amaliebanan <qtb931@alumni.ku.dk>
2021-02-10 21:28:37 +0100

    hehe lidt mere

--- BachelorProjekt/AgentClass.py
+++ BachelorProjekt/AgentClass.py
@@ -5,13 +5,21 @@ import numpy as np
 class covid_Agent(Agent):
     def __init__(self,id, model):
         super().__init__(id, model)
-        self.infected = False
+        self.infected = 0 #0 for False, 1 for True
         self.id = id
 
     #The step method is the action the agent takes when it is activated by the model schedule.
     def step(self):
         #to be implemented
         #whatever the agent does when activated
-        print("Hi, my ID is",str(self.id))
+        print("Hi, my ID is",self.id,"Do I have covid?:", self.infected)
+
+        #Infect another agent random
+        if self.infected == 0:
+            return
+        other_agent = self.random.choice(self.model.schedule.agents)
+        other_agent.infected = 1
+        self.infected = 1
+
 
 
--- BachelorProjekt/Model.py
+++ BachelorProjekt/Model.py
@@ -1,17 +1,27 @@
 import AgentClass as ac
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
+import matplotlib.pyplot as plt
 from mesa import Agent, Model
 
 class covid_Model(Model):
     def __init__(self,N):
         self.n_agents = N
         self.schedule = RandomActivation(self)
+
         #The scheduler is a special model component which controls the order in which agents are activated
-        for i in range(self.n_agents):
+        for i in range(1,self.n_agents+1):
             newAgent = ac.covid_Agent(i,self)
-            #add agent to scheduler
+            #Add agent to scheduler
             self.schedule.add(newAgent)
+
+        #Tilføj en positiv agenter (fjern fra oprindelig liste af agenter, for at ændre infected-status)
+        randomAgent = self.random.choice(self.schedule.agents)
+        self.schedule.remove(randomAgent)
+
+        postive_agent = randomAgent
+        postive_agent.infected = 1
+        self.schedule.add(postive_agent)
 
     def step(self):
         self.schedule.step()
