e89e9c156f2cdb95eea91ca6f0dc277e1f6e757c
amaliebanan <qtb931@alumni.ku.dk>
2021-02-12 13:18:19 +0100

    grid

--- BachelorProjekt/AgentClass.py
+++ BachelorProjekt/AgentClass.py
@@ -1,5 +1,5 @@
 from mesa import Agent, Model
-import random
+from mesa.space import MultiGrid
 import numpy as np
 
 
@@ -7,12 +7,28 @@ class covid_Agent(Agent):
     def __init__(self,id, model):
         super().__init__(id, model)
         self.infected = 0 #0 for False, 1 for True
-        self.pos = 0
         self.infection_period = 9
         self.id = id
 
+    # If other_agent is already infected, find a new one
+    def infectNewAgent(self):
+        other_agent = self.random.choice(self.model.schedule.agents)
+        while other_agent.infected == 1:
+            other_agent = self.random.choice(self.model.schedule.agents)
+        other_agent.infected = 1
+
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=False
+        )
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
     #The step method is the action the agent takes when it is activated by the model schedule.
     def step(self):
+
         #whatever the agent does when activated
         #print("ID",self.id, "Status:", self.infected)
         #Infect another agent random
@@ -20,13 +36,11 @@ class covid_Agent(Agent):
             return
 
 
-        #Generate random int and infect a new agent if int = 1. If other_agent is already infected, find a new one
+        #Generate random int and infect a new agent if int = 1.
+        #
         randomInt = np.random.poisson(1/8)
         if randomInt == 1:
-            other_agent = self.random.choice(self.model.schedule.agents)
-            while other_agent.infected == 1:
-                other_agent = self.random.choice(self.model.schedule.agents)
-            other_agent.infected = 1
+            self.infectNewAgent()
 
         #Subtract 1 from infection period. Reset infection period if == 0 and make agent not infected anymore
         # (not yet immun)
--- BachelorProjekt/Model.py
+++ BachelorProjekt/Model.py
@@ -3,10 +3,12 @@ from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 import matplotlib.pyplot as plt
 from mesa import Agent, Model
+import numpy as np
 
 class covid_Model(Model):
-    def __init__(self,N):
+    def __init__(self,N,height, width):
         self.n_agents = N
+        self.grid = MultiGrid(width,height,True)
         self.schedule = RandomActivation(self)
 
         #The scheduler is a special model component which controls the order in which agents are activated
@@ -14,6 +16,11 @@ class covid_Model(Model):
             newAgent = ac.covid_Agent(i,self)
             #Add agent to scheduler
             self.schedule.add(newAgent)
+
+            #Place agent randomly on grid
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(newAgent, (x, y))
 
         #Tilføj en positiv agenter (fjern fra oprindelig liste af agenter, for at ændre infected-status)
         for i in range(0,7):
@@ -26,14 +33,10 @@ class covid_Model(Model):
     def step(self):
         self.schedule.step()
 
-
-
 all_agents = []
 timesteps = []
-#for i in range(1):
 
-
-myModel = covid_Model(100)
+myModel = covid_Model(60,10,10)
 for j in range(50):
     counter = 0
     myModel.step()
@@ -47,4 +50,16 @@ agents_status = [agent.infected for agent in myModel.schedule.agents]
 print(timesteps)
 
 #plt.hist(all_agents)
+#plt.show()
+
+agents_status = np.zeros((myModel.grid.width, myModel.grid.height))
+for cell in myModel.grid.coord_iter():
+    cell_content, x, y = cell
+    if type(cell_content) == ac.covid_Agent:
+        status = cell_content[0].infected
+    else: status = 2
+    agents_status[x][y] = status
+
+plt.imshow(agents_status)
+plt.colorbar()
 #plt.show()
